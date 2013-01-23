1e273c8dc1f79e940acaeb85b521a777e3e39cce
Christopher Clarke <cclarke@chrisdev.com>
2013-01-23 06:58:19 -0400

    Fixed term get_absoloute_url and got rid of the permalink decorator

--- glossary/models.py
+++ glossary/models.py
@@ -1,23 +1,30 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 class Term(models.Model):
-    title       = models.CharField(max_length=250)
-    slug        = models.SlugField(unique=True)
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(unique=True)
     description = models.TextField()
 
     class Meta:
         ordering = ['title']
 
-    @models.permalink
     def get_absolute_url(self):
-       return ('glossary_detail', (), {'slug': self.slug})
+        name = "glossary-detail"
+        kwargs = {
+            "slug": self.slug,
+        }
+
+        return reverse(name, kwargs=kwargs)
+
 
     def __unicode__(self):
         return unicode(self.title)
 
+
 class Synonym(models.Model):
     title = models.CharField(max_length=250)
-    term  = models.ForeignKey(Term, related_name="synonyms")
+    term = models.ForeignKey(Term, related_name="synonyms")
 
     def __unicode__(self):
         return u"%s (synonym for %s)" % (self.title, self.term.title)
